tytso/e2fsprogs

commit e3507739e4185bdb2394928eb6479e48f4e690a8

Fix gcc -Wall nits
This fixes the last set of gcc -Wall complaints.

Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

e2fsck/recovery.c
@@ -283,12 +283,9 @@ int journal_recover(journal_t *journal)
 int journal_skip_recovery(journal_t *journal)
 {
 	int			err;
-	journal_superblock_t *	sb;
-
 	struct recovery_info	info;
 
 	memset (&info, 0, sizeof(info));
-	sb = journal->j_superblock;
 
 	err = do_one_pass(journal, &info, PASS_SCAN);
 
@@ -362,11 +359,6 @@ static int do_one_pass(journal_t *journal,
 	int			blocktype;
 	int			tag_bytes = journal_tag_bytes(journal);
 	__u32			crc32_sum = ~0; /* Transactional Checksums */
-
-	/* Precompute the maximum metadata descriptors in a descriptor block */
-	int			MAX_BLOCKS_PER_DESC;
-	MAX_BLOCKS_PER_DESC = ((journal->j_blocksize-sizeof(journal_header_t))
-			       / tag_bytes);
 
 	/*
 	 * First thing is to establish what we expect to find in the log

debugfs/extent_inode.c
@@ -66,9 +66,8 @@ static char *orig_prompt, *extent_prompt;
 
 void do_extent_open(int argc, char *argv[])
 {
-	struct ext3_extent_header *eh;
 	ext2_ino_t	inode;
-	int		i, ret;
+	int		ret;
 	errcode_t	retval;
 	char		*cp;
 
@@ -220,7 +219,6 @@ void do_delete_node(int argc, char *argv[])
 {
 	struct ext2fs_extent extent;
 	errcode_t	retval;
-	int		err;
 
 	if (common_extent_args_process(argc, argv, 1, 1, "delete_node",
 				       "", CHECK_FS_RW | CHECK_FS_BITMAPS))
@@ -286,8 +284,6 @@ void do_replace_node(int argc, char *argv[])
 void do_split_node(int argc, char *argv[])
 {
 	errcode_t	retval;
-	struct ext2fs_extent extent;
-	int err;
 
 	if (common_extent_args_process(argc, argv, 1, 1, "split_node",
 				       "", CHECK_FS_RW | CHECK_FS_BITMAPS))
@@ -465,8 +461,6 @@ void do_print_all(int argc, char **argv)
 
 void do_fix_parents(int argc, char **argv)
 {
-	struct ext2fs_extent	extent;
-	struct ext2_extent_info	info;
 	errcode_t		retval;
 
 	if (common_extent_args_process(argc, argv, 1, 1, "fix_parents", "",
@@ -515,9 +509,7 @@ void do_info(int argc, char **argv)
 
 void do_goto_block(int argc, char **argv)
 {
-	struct ext2fs_extent	extent;
 	errcode_t		retval;
-	int			op = EXT2_EXTENT_NEXT_LEAF;
 	blk64_t			blk;
 	int			level = 0, err;
 

debugfs/htree.c
@@ -201,7 +201,6 @@ void do_htree_dump(int argc, char *argv[])
 	char		*buf = NULL;
 	struct 		ext2_dx_root_info  *rootnode;
 	struct 		ext2_dx_entry *ent;
-	struct		ext2_dx_countlimit *limit;
 	errcode_t	errcode;
 
 	if (check_fs_open(argv[0]))
@@ -255,7 +254,6 @@ void do_htree_dump(int argc, char *argv[])
 	fprintf(pager, "\t Flags: %d\n", rootnode->unused_flags);
 
 	ent = (struct ext2_dx_entry *) (buf + 24 + rootnode->info_length);
-	limit = (struct ext2_dx_countlimit *) ent;
 
 	htree_dump_int_node(current_fs, ino, &inode, rootnode, ent,
 			    buf + current_fs->blocksize,

debugfs/zap.c
@@ -32,9 +32,7 @@ void do_zap_block(int argc, char *argv[])
 	ext2_ino_t	inode;
 	errcode_t	errcode;
 	blk64_t		block;
-	int		retval;
 	char		*file = NULL;
-	char		*in_file = NULL;
 	int		c, err;
 	int		offset = -1;
 	int		length = -1;
@@ -171,14 +169,11 @@ errout:
 
 void do_dump_block(int argc, char *argv[])
 {
-	unsigned long	pattern = 0;
 	unsigned char	*buf;
 	ext2_ino_t	inode;
 	errcode_t	errcode;
 	blk64_t		block;
-	int		retval;
 	char		*file = NULL;
-	char		*in_file = NULL;
 	int		c, err, i, j;
 	int		suppress = -1;
 

e2fsck/pass1.c
@@ -274,7 +274,7 @@ static void check_ea_in_inode(e2fsck_t ctx, struct problem_context *pctx)
 	struct ext2_super_block *sb = ctx->fs->super;
 	struct ext2_inode_large *inode;
 	struct ext2_ext_attr_entry *entry;
-	char *start, *end;
+	char *start;
 	unsigned int storage_size, remain;
 	int problem = 0;
 
@@ -283,7 +283,6 @@ static void check_ea_in_inode(e2fsck_t ctx, struct problem_context *pctx)
 		inode->i_extra_isize;
 	start = ((char *) inode) + EXT2_GOOD_OLD_INODE_SIZE +
 		inode->i_extra_isize + sizeof(__u32);
-	end = (char *) inode + EXT2_INODE_SIZE(ctx->fs->super);
 	entry = (struct ext2_ext_attr_entry *) start;
 
 	/* scan all entry's headers first */

e2fsck/rehash.c
@@ -828,7 +828,7 @@ void e2fsck_rehash_directories(e2fsck_t ctx)
 	struct dir_info_iter *	dirinfo_iter = 0;
 	ext2_ino_t		ino;
 	errcode_t		retval;
-	int			cur, max, all_dirs, dir_index, first = 1;
+	int			cur, max, all_dirs, first = 1;
 
 	init_resource_track(&rtrack, ctx->fs->io);
 	all_dirs = ctx->options & E2F_OPT_COMPRESS_DIRS;
@@ -840,7 +840,6 @@ void e2fsck_rehash_directories(e2fsck_t ctx)
 
 	clear_problem_context(&pctx);
 
-	dir_index = ctx->fs->super->s_feature_compat & EXT2_FEATURE_COMPAT_DIR_INDEX;
 	cur = 0;
 	if (all_dirs) {
 		dirinfo_iter = e2fsck_dir_info_iter_begin(ctx);

e2fsck/unix.c
@@ -204,8 +204,7 @@ static void show_stats(e2fsck_t	ctx)
 			ctx->fs_blockdev_count), ctx->fs_blockdev_count);
 	log_out(ctx, P_("%12u fifo\n", "%12u fifos\n", ctx->fs_fifo_count),
 		ctx->fs_fifo_count);
-	log_out(ctx, P_("%12u link\n", "%12u links\n",
-			ctx->fs_links_count - dir_links),
+	log_out(ctx, P_("%12u link\n", "%12u links\n", num_links),
 		ctx->fs_links_count - dir_links);
 	log_out(ctx, P_("%12u symbolic link", "%12u symbolic links",
 			ctx->fs_symlinks_count), ctx->fs_symlinks_count);
@@ -217,9 +216,8 @@ static void show_stats(e2fsck_t	ctx)
 			ctx->fs_sockets_count),
 		ctx->fs_sockets_count);
 	log_out(ctx, "------------\n");
-	log_out(ctx, P_("%12u file\n", "%12u files\n",
-			ctx->fs_total_count - dir_links),
-		ctx->fs_total_count - dir_links);
+	log_out(ctx, P_("%12u file\n", "%12u files\n", num_files),
+		num_files);
 }
 
 static void check_mount(e2fsck_t ctx)

e2fsck/util.c
@@ -616,7 +616,7 @@ int ext2_file_type(unsigned int mode)
 errcode_t e2fsck_zero_blocks(ext2_filsys fs, blk_t blk, int num,
 			     blk_t *ret_blk, int *ret_count)
 {
-	int		j, count, next_update, next_update_incr;
+	int		j, count;
 	static char	*buf;
 	errcode_t	retval;
 
@@ -639,10 +639,6 @@ errcode_t e2fsck_zero_blocks(ext2_filsys fs, blk_t blk, int num,
 		memset(buf, 0, fs->blocksize * STRIDE_LENGTH);
 	}
 	/* OK, do the write loop */
-	next_update = 0;
-	next_update_incr = num / 100;
-	if (next_update_incr < 1)
-		next_update_incr = 1;
 	for (j = 0; j < num; j += STRIDE_LENGTH, blk += STRIDE_LENGTH) {
 		count = num - j;
 		if (count > STRIDE_LENGTH)

MotorolaMobilityLLC/external-e2fsprogs

commit 13507cdcb39340c13a569fc9e46e49a8780ba718

e2fsck: fix build failure with --enable-jbd-debug
Commit e3507739e4185 introduced a build failure if e2fsprogs is
configured with --enable-jbd-debug.  Fix this.

Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

e2fsck/recovery.c
@@ -294,6 +294,8 @@ int journal_skip_recovery(journal_t *journal)
 		++journal->j_transaction_sequence;
 	} else {
 #ifdef CONFIG_JBD_DEBUG
+		journal_superblock_t *sb = journal->j_superblock;
+
 		int dropped = info.end_transaction - be32_to_cpu(sb->s_sequence);
 #endif
 		jbd_debug(1,